7b8f8dd78d679b337d4e2d2aa19ba8b508fed1bf
Tuấn Hwing <tuanhwing2014@gmail.com>
2023-08-04 11:03:25 +0700

    add helper contains redis + parseVersion functions

--- go.mod
+++ go.mod
@@ -11,6 +11,11 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+)
+
+require (
 	cloud.google.com/go v0.110.4 // indirect
 	cloud.google.com/go/compute v1.20.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
@@ -18,6 +23,8 @@ require (
 	cloud.google.com/go/iam v1.1.0 // indirect
 	cloud.google.com/go/longrunning v0.5.1 // indirect
 	cloud.google.com/go/storage v1.31.0 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
---
+++ helper.go
@@ -0,0 +1,73 @@
+package gotercore
+
+import (
+	"context"
+	"encoding/json"
+	"strconv"
+	"strings"
+
+	"github.com/go-redis/redis/v8"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func RegisSet(c redis.Client, key string, value interface{}) error {
+	p, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return c.Set(context.Background(), key, p, 0).Err()
+}
+
+func RegisGet(c redis.Client, key string, dest interface{}) error {
+	p, err := c.Get(context.Background(), key).Bytes()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(p, dest)
+}
+
+func RegisDel(c *redis.Client, key string) error {
+	return c.Del(context.Background(), key).Err()
+}
+
+func ToDoc(v interface{}) (doc bson.D, err error) {
+	data, err := bson.Marshal(v)
+	if err != nil {
+		return
+	}
+
+	err = bson.Unmarshal(data, &doc)
+	return
+}
+
+func ParseAppVersion(s string) [4]int {
+	var results [4]int
+
+	parts := strings.Split(s, ".")
+	if len(parts) < 3 {
+		return results
+	}
+
+	nameAndBuild := strings.Split(parts[2], "+")
+	if len(nameAndBuild) < 2 {
+		return results
+	}
+
+	//Version number Part 1
+	versionNumber1, _ := strconv.Atoi(parts[0])
+	results[0] = versionNumber1
+
+	//Version number Part 2
+	versionNumber2, _ := strconv.Atoi(parts[1])
+	results[1] = versionNumber2
+
+	//Version number Part 3
+	versionNumber3, _ := strconv.Atoi(nameAndBuild[0])
+	results[2] = versionNumber3
+
+	//Build number
+	buildNumber, _ := strconv.Atoi(nameAndBuild[1])
+	results[3] = buildNumber
+
+	return results
+}
